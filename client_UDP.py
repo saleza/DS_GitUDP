4f0073b6b8601d1f0e090e11fb6cb4498fc5f33d
Adrien <saleza@lacroixrouge-brest.fr>
2017-10-12 13:55:53 +0200

    Test du dépot

--- client_UDP.py
+++ client_UDP.py
@@ -2,20 +2,26 @@
 
 import socket
 
+UDP_IP = "192.168.0.202"
+UDP_PORT = 5005
+
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.settimeout(1.0)
 
-sock.connect(???)
-sock.send(???)
+sock.connect(UDP_IP,UDP_PORT)
+sock.send("cinema",UDP_IP, UDP_PORT)
 
 
 trameReponse, addr = sock.recvfrom(1024)
 
 print "Réception de la trame de réponse", trameReponse.encode("hex")
 
-code = ???
+code = (float)(trameReponse[3] << 24)
+code = code + (trameReponse[4] << 16)
+code = code + (trameReponse[5] << 8)
+code = code + 
 
 print code
 
